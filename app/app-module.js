770dcda8313caea38a2d48a3ecdca68e1c4fbc38
Razvan Pavel <razvan.pavel@akqa.com>
2016-07-29 00:51:45 +0200

    hello angular app with jade & stylus

---
+++ app/app-module.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name votDiaspora
+   * @description
+   *
+   */
+  angular
+    .module('votDiaspora', [
+      'ui.router',
+      'mm.foundation'
+    ]);
+}());
