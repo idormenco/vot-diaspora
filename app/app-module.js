11ebef9a297b091bbe5826e854e4931138ca6d27
Octavian David <octavian.david@komunomo.com>
2016-07-29 20:19:28 +0300

    test homepage

--- app/app-module.js
+++ app/app-module.js
@@ -9,6 +9,33 @@
   angular
     .module('votDiaspora', [
       'ui.router',
-      'mm.foundation'
-    ]);
+      'mm.foundation',
+      'uiGmapgoogle-maps',
+      // 'ngStorage'
+      // 'ngGeolocation'
+      // 'angular-google-maps-geocoder'
+    ])
+    .config(function (uiGmapGoogleMapApiProvider) {
+      uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyCmgEeIhijJe689d_bpjPX9Sf1Bsm5fidU',
+        v: '3.23',
+        libraries: 'weather,geometry,visualization'
+      });
+    })
+    .controller('DiasporaCtrl', function ($scope, $state, uiGmapGoogleMapApi) {
+      var vm = this;
+      vm.state = $state;
+
+      uiGmapGoogleMapApi.then(function (maps) {
+        maps.visualRefresh = true;
+        vm.map = {
+          center: {
+            latitude: 0,
+            longitude: 0
+          },
+          zoom: 3
+        };
+        console.log(vm);
+      });
+    });
 }());
--- app/app-routes.js
+++ app/app-routes.js
@@ -5,7 +5,23 @@
     .module('votDiaspora')
     .config(config);
 
-  function config($urlRouterProvider) {
+  function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'home.html',
+          controller: 'DiasporaCtrl as vm'
+        })
+        .state('test1', {
+          url: '/test1',
+          templateUrl: 'test1.html',
+          controller: 'DiasporaCtrl as vm'
+        })
+        .state('test2', {
+          url: '/test2',
+          templateUrl: 'test2.html',
+          controller: 'DiasporaCtrl as vm'
+        });
     $urlRouterProvider.otherwise('/home');
   }
 }());
